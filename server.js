59658cd09ca5d355d48e5cad250e7d085a47ef5e
Scottie <avscott@pacbell.net>
2019-11-30 14:31:36 -0800

    connect server

---
+++ server.js
@@ -0,0 +1,34 @@
+// Require Dependencies
+let express = require("express");
+let path = require("path");
+let mongoose = require("mongoose");
+let axios = require("axios");
+let cheerio = require("cheerio");
+
+// Set up the Express App
+let app = express();
+let PORT = process.env.PORT || 3020;
+
+app.use(express.static(__dirname + "/public"));
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up Handlebars
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set up route
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
