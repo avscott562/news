e4f3655a8ae0a384d259376d5ac8bf7c1a34a450
Scottie <avscott@pacbell.net>
2019-12-11 08:01:47 -0800

    move article view

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,17 @@
 import Axios from "axios";
 
-$("scrapeBtn").on("click", function() {
+$("#scrapeBtn").on("click", function() {
     // ajax request to /scrape
 });
 
-$(".clearBtn").on("click", function() {
-    // ajax request to delete articles
+$("#clearBtn").on("click", function() {
+    // delete articles from screen
+});
+
+$("#notes").on("click", function() {
+    // open notes on saved articles
+});
+
+$("#delete").on("click", function() {
+    // delete from saved articles
 });
